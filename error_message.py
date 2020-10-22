c92bf7a57093af763a002b51f7ac7fd51c0613af
stonesaw <ishichan1260@gmail.com>
2020-10-23 02:54:08 +0900

    delete error_message.py

--- error_message.py
+++
@@ -1,6 +0,0 @@
-error = {
-    "exec-arithmetic-error": "算術上のエラーがあります",
-    "exec-name-error": "定義されてない変数があります",
-    "exec-syntax-error": "構文エラーが発生しました",
-    "exec-exception": "予期せぬエラーが発生しました"
-}
